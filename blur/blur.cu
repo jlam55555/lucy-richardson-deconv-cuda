3f26b5d4f7b23ce03e3e30d17c2b77cd28966983
Jonathan Lam <jlam55555@gmail.com>
2021-03-15 18:01:28 -0400

    moved old blur

---
+++ blur/blur.cu
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+__host__ int main(void)
+{
+	printf("Hello, world!\n");
+	return 0;
+}
---
+++ deblur/deblur.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+
+__host__ int main(void)
+{
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+}
