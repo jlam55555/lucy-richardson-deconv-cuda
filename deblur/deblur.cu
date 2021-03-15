619a508ca8625591088b56b73777c2e96ecedfef
Jonathan Lam <jlam55555@gmail.com>
2021-03-15 18:31:05 -0400

    added libpng example

--- deblur/deblur.cu
+++ deblur/deblur.cu
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "png.h"
 
 __host__ int main(void)
 {
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Hello, world!\n" << std::endl;
 
+	std::cout << "Reading file..." << std::endl;
+	read_png_file("test.png");
+
+	std::cout << "Writing file..." << std::endl;
+	write_png_file("out.png");
+
+	std::cout << "Done." << std::endl;
 	return 0;
 }
